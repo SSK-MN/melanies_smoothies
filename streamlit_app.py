29613ade1d531652d73e83e87792e10564a136e2
SSK-MN <sasikran55023@gmail.com>
2026-07-01 15:49:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,14 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 
-# เพิ่มบรรทัดนี้
-ingredients_string = ''
-
 if ingredients_list:
-    for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-
-    #st.write(ingredients_string)
-
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+  ingredients_string = ''
+  for fruit_chosen in ingredients_list:
+      ingredients_string += fruit_chosen + ' '
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+    
+  my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','"""+ name_on_order +"""')"""
 
 #st.write(my_insert_stmt)
@@ -47,7 +45,5 @@ if time_to_insert:
     st.success('Your Smoothie is ordered!', icon="✅")
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
